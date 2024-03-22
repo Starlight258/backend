fed2db6a49866ae0d8d1c1aca6f3a64182a27fc0
robinjoon <robin980108@naver.com>
2024-03-22 15:27:12 +0900

    feat: 문서 조회 기능 구현

---
+++ src/main/java/com/wooteco/wiki/dto/ErrorResponse.java
@@ -0,0 +1,4 @@
+package com.wooteco.wiki.dto;
+
+public record ErrorResponse(String errorMessage) {
+}
--- src/main/java/com/wooteco/wiki/service/PostService.java
+++ src/main/java/com/wooteco/wiki/service/PostService.java
@@ -2,7 +2,10 @@ package com.wooteco.wiki.service;
 
 import com.wooteco.wiki.dto.PostCreateRequest;
 import com.wooteco.wiki.dto.PostResponse;
+import java.util.Optional;
 
 public interface PostService {
     PostResponse post(String title, PostCreateRequest postCreateRequest);
+
+    Optional<PostResponse> get(String title);
 }
