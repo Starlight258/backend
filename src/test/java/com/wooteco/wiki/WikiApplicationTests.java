d6bc1c713aa440ed078086e2c3b5c51c08db9dcb
robinjoon <robin980108@naver.com>
2024-03-25 23:09:59 +0900

    fix(WikiApplicationTests): 테스트가 수행되지 않는 오류 수정

--- src/test/java/com/wooteco/wiki/WikiApplicationTests.java
+++ src/test/java/com/wooteco/wiki/WikiApplicationTests.java
@@ -2,12 +2,14 @@ package com.wooteco.wiki;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class WikiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
