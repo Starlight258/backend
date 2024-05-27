166bc477489cd13d09ff20c281aae47b2ff08d19
robinjoon <robin980108@naver.com>
2024-05-27 21:56:21 +0900

    refactor: TokenManager 의 시크릿 키를 외부로 분리하기 위해 Bean 으로 변경

--- src/main/java/com/wooteco/wiki/domain/TokenManager.java
+++ src/main/java/com/wooteco/wiki/domain/TokenManager.java
@@ -1,8 +1,13 @@
 package com.wooteco.wiki.domain;
 
 import com.wooteco.wiki.exception.WrongTokenException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TokenManager {
+    @Value("${jwt.key}")
+    private String secretKey;
 
     /**
      * 생성된 엑세스 토큰은 리프레쉬 토큰으로 사용할 수 없어야 함. 엑세스 토큰에는 회원의 식별자를 제외한 다른 개인 정보가 포함되면 안됨.
@@ -10,7 +15,7 @@ public class TokenManager {
      * @param member 회원 도메인
      * @return 엑세스 토큰
      */
-    public static String generateAccessToken(Member member) {
+    public String generateAccessToken(Member member) {
         return null;
     }
 
@@ -20,7 +25,7 @@ public class TokenManager {
      * @param member 회원 도메인
      * @return 리프레쉬 토큰
      */
-    public static String generateRefreshToken(Member member) {
+    public String generateRefreshToken(Member member) {
         return null;
     }
 
@@ -31,7 +36,7 @@ public class TokenManager {
      * @return 회원의 식별자.
      * @throws WrongTokenException 엑세스 토큰에 문제(잘못된 토큰, 기간 만료 등)가 있어 회원 식별자를 추출할 수 없는 경우
      */
-    public static long extractMemberId(String accessToken) throws WrongTokenException {
+    public long extractMemberId(String accessToken) throws WrongTokenException {
         return -1;
     }
 }
