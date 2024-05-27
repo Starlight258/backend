8cefee86cbb55ba42c838a59283e54ff702fa1d6
robinjoon <robin980108@naver.com>
2024-05-27 21:01:02 +0900

    feat: MemberRepository 추가

---
+++ src/main/java/com/wooteco/wiki/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.wooteco.wiki.repository;
+
+import com.wooteco.wiki.entity.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByEmailAndPassword(String email, String password);
+}
