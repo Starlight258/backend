b6c87069d3dd910a4f9d91ea0fa0dd4bba846604
robinjoon <robin980108@naver.com>
2024-05-27 21:02:18 +0900

    fix: 트랜잭션 추가

--- src/main/java/com/wooteco/wiki/service/DocumentSearchService.java
+++ src/main/java/com/wooteco/wiki/service/DocumentSearchService.java
@@ -5,8 +5,10 @@ import com.wooteco.wiki.repository.DocumentRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class DocumentSearchService {
     private final DocumentRepository documentRepository;
