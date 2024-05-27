aac7deeb6f8de8e6b81e59c160bb76fd9152508b
robinjoon <robin980108@naver.com>
2024-05-27 21:00:49 +0900

    refactor: config 클래스 위치 이동

--- src/main/java/com/wooteco/wiki/config/WebConfig.java
+++ src/main/java/com/wooteco/wiki/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.wooteco.wiki.auth.configure;
+package com.wooteco.wiki.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
