72fdef40ace8081102ccbf6e6f23d080116f50b3
robinjoon <robin980108@naver.com>
2024-05-29 20:32:29 +0900

    test: 비번 제약 조건 테스트 추가

---
+++ src/test/java/com/wooteco/wiki/domain/PasswordTest.java
@@ -0,0 +1,17 @@
+package com.wooteco.wiki.domain;
+
+import com.wooteco.wiki.exception.WrongPasswordException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PasswordTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"1234", "1234a", "qwer1@", "123456A", "01234567890123456789a"})
+    @DisplayName("비밀번호 정책에 맞지 않는 것이 생성되지 않는지 확인")
+    void failWhenWrongPassword(String password) {
+        Assertions.assertThatThrownBy(() -> new Password(password))
+                .isInstanceOf(WrongPasswordException.class);
+    }
+}
