d8d00645cab99fe3e325a85551182f754405dbaa
robinjoon <robin980108@naver.com>
2024-06-10 14:53:58 +0900

    fix: 로그인 및 회원 가입 시 엑세스 토큰을 쿠키에 넣도록 수정

--- src/main/java/com/wooteco/wiki/controller/AuthController.java
+++ src/main/java/com/wooteco/wiki/controller/AuthController.java
@@ -4,7 +4,12 @@ import com.wooteco.wiki.dto.AuthTokens;
 import com.wooteco.wiki.dto.JoinRequest;
 import com.wooteco.wiki.dto.LoginRequest;
 import com.wooteco.wiki.service.AuthService;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +20,27 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public AuthTokens login(@RequestBody LoginRequest loginRequest) {
-        return authService.login(loginRequest);
+    public ResponseEntity<AuthTokens> login(@RequestBody LoginRequest loginRequest) {
+        AuthTokens authTokens = authService.login(loginRequest);
+        return makeResponseEntityWithTokenCookie(authTokens);
+    }
+
+    private ResponseEntity<AuthTokens> makeResponseEntityWithTokenCookie(AuthTokens authTokens) {
+        LocalDateTime now = LocalDateTime.now();
+        Duration tokenLifeTime = Duration.between(now, now.plusHours(1));
+        String accessToken = authTokens.accessToken();
+        ResponseCookie cookie = ResponseCookie.from("token", accessToken)
+                .httpOnly(true)
+                .maxAge(tokenLifeTime)
+                .build();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(authTokens);
     }
 
     @PostMapping("/join")
-    public AuthTokens join(@RequestBody JoinRequest joinRequest) {
-        return authService.join(joinRequest);
+    public ResponseEntity<AuthTokens> join(@RequestBody JoinRequest joinRequest) {
+        AuthTokens authTokens = authService.join(joinRequest);
+        return makeResponseEntityWithTokenCookie(authTokens);
     }
 }
