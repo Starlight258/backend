7746af1c9c28bee5f6ddf72e80b7b5622ca602a8
Minjoo <m0522inju@gmail.com>
2025-03-13 15:42:16 +0900

    refactor: DocumentRepository 코틀린으로 변환

---
+++ src/main/java/com/wooteco/wiki/repository/DocumentRepository.kt
@@ -0,0 +1,16 @@
+package com.wooteco.wiki.repository
+
+import com.wooteco.wiki.domain.Document
+import com.wooteco.wiki.domain.Title
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+
+interface DocumentRepository : JpaRepository<Document, Long> {
+    fun findByTitle(title: String): Optional<Document>
+
+    fun existsByTitle(title: String): Boolean
+
+    fun findAllByOrderByGenerateTimeDesc(): List<Document>
+
+    fun findAllByTitleStartingWith(keyWord: String): List<Title>
+}
