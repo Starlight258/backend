9988bede2a1267aa99b4e6b0a7f2db68bb5cb1e4
2jin2.1031 <2jin2.1031@gmail.com>
2025-06-01 22:24:01 +0900

    feat: s3 업로드 기능 구현

---
+++ src/main/java/com/wooteco/wiki/config/aws/AwsS3Uploader.java
@@ -0,0 +1,59 @@
+package com.wooteco.wiki.config.aws;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class AwsS3Uploader {
+
+    @Autowired
+    private final S3Presigner s3Presigner;
+
+    private static final String BUCKET_DOMAIN = "https://crewwiki7th.s3.ap-northeast-2.amazonaws.com/";
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    /**
+     * presigned url을 생성해 주는 메소드. bucket v3에 생성해 줌
+     * @param imageExtension 이미지의 확장자
+     * @return upload url, public url
+     */
+    public String createPresignedUrl(String imageExtension) {
+
+        String keyName = UUID.randomUUID() + "." + imageExtension;
+        keyName = keyName.replace("-", "");
+        String contentType = "image/" + imageExtension;
+        Map<String, String> metadata = Map.of(
+                "fileType", contentType,
+                "Content-Type", contentType
+        );
+
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(keyName)
+                .metadata(metadata)
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))  // The URL expires in 10 minutes.
+                .putObjectRequest(objectRequest)
+                .build();
+
+
+        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(presignRequest);
+        return presignedRequest.url().toString();
+    }
+}
