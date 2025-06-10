bcd38fe3d403e5b17f45f7b5b33f8ae8ac7880a8
2jin2.1031 <2jin2.1031@gmail.com>
2025-06-10 22:32:32 +0900

    refactor: 불필요한 주석 제거

--- src/main/java/com/wooteco/wiki/config/aws/AwsS3Uploader.java
+++ src/main/java/com/wooteco/wiki/config/aws/AwsS3Uploader.java
@@ -21,11 +21,6 @@ public class AwsS3Uploader {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    /**
-     * presigned url을 생성해 주는 메소드. bucket v3에 생성해 줌
-     * @param fileKey 이미지의 확장자
-     * @return upload url, public url
-     */
     public String createPresignedUrl(String fileKey) {
         String contentType = "image/jpeg";
         Map<String, String> metadata = Map.of(
@@ -40,10 +35,9 @@ public class AwsS3Uploader {
                 .build();
 
         PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
-                .signatureDuration(Duration.ofMinutes(10))  // The URL expires in 10 minutes.
+                .signatureDuration(Duration.ofMinutes(10))
                 .putObjectRequest(objectRequest)
                 .build();
-
 
         PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(presignRequest);
         return presignedRequest.url().toString();
