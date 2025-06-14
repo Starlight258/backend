3a1cc4e84726168b1ad77d9ee244e6b7b9c23282
2jin2.1031 <2jin2.1031@gmail.com>
2025-06-14 21:09:18 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/com/wooteco/wiki/document/controller/DocumentController.kt
+++ src/main/java/com/wooteco/wiki/document/controller/DocumentController.kt
@@ -2,17 +2,16 @@ package com.wooteco.wiki.document.controller
 
 import com.wooteco.wiki.document.domain.Document
 import com.wooteco.wiki.document.domain.dto.DocumentCreateRequest
-import com.wooteco.wiki.document.domain.dto.DocumentFindAllByRecentResponse
 import com.wooteco.wiki.document.domain.dto.DocumentResponse
 import com.wooteco.wiki.document.domain.dto.DocumentUpdateRequest
 import com.wooteco.wiki.document.domain.dto.DocumentUuidResponse
-import com.wooteco.wiki.log.domain.dto.LogDetailResponse
-import com.wooteco.wiki.log.domain.dto.LogResponse
 import com.wooteco.wiki.document.service.DocumentSearchService
 import com.wooteco.wiki.document.service.DocumentService
 import com.wooteco.wiki.document.service.UUIDService
 import com.wooteco.wiki.global.common.PageRequestDto
 import com.wooteco.wiki.global.common.ResponseDto
+import com.wooteco.wiki.log.domain.dto.LogDetailResponse
+import com.wooteco.wiki.log.domain.dto.LogResponse
 import com.wooteco.wiki.log.service.LogService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
