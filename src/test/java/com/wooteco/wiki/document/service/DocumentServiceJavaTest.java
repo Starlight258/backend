dc1929ce06910fb87dc5d2c831c264e3f587a9b0
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-30 06:37:41 +0900

    test: uuid가 존재하지 않을 때의 예외 테스트 추가

--- src/test/java/com/wooteco/wiki/document/service/DocumentServiceJavaTest.java
+++ src/test/java/com/wooteco/wiki/document/service/DocumentServiceJavaTest.java
@@ -2,11 +2,14 @@ package com.wooteco.wiki.document.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.wooteco.wiki.document.domain.Document;
 import com.wooteco.wiki.document.dto.DocumentOrganizationAddRequest;
 import com.wooteco.wiki.document.fixture.DocumentFixture;
 import com.wooteco.wiki.document.repository.DocumentRepository;
+import com.wooteco.wiki.global.exception.ErrorCode;
+import com.wooteco.wiki.global.exception.WikiException;
 import com.wooteco.wiki.organizationdocument.domain.OrganizationDocument;
 import com.wooteco.wiki.organizationdocument.dto.OrganizationDocumentSearchResponse;
 import com.wooteco.wiki.organizationdocument.dto.response.OrganizationDocumentResponse;
@@ -16,6 +19,7 @@ import com.wooteco.wiki.organizationdocument.service.DocumentOrganizationLinkSer
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -52,7 +56,7 @@ class DocumentServiceJavaTest {
 
     @DisplayName("특정 문서에 대한 조직 문서 제목과 Uuid들을 조회할 때")
     @Nested
-    class readOrganizationTitleAndUuid {
+    class searchOrganizationDocument {
 
         @BeforeEach
         void setUp() {
@@ -71,9 +75,9 @@ class DocumentServiceJavaTest {
 
         @DisplayName("존재하는 특정 문서의 Uuid로 요청한다면 조직 문서들이 올바르게 조회된다")
         @Test
-        void readOrganizationTitleAndUuid_success_byExistingDocumentUuid() {
+        void searchOrganizationDocument_success_byExistingDocumentUuid() {
             // when
-            List<OrganizationDocumentSearchResponse> documentOrganizationSearchRespons = documentService.readOrganizationTitleAndUuid(
+            List<OrganizationDocumentSearchResponse> documentOrganizationSearchRespons = documentService.searchOrganizationDocument(
                     savedDocumentUuid);
 
             // then
@@ -85,22 +89,35 @@ class DocumentServiceJavaTest {
                     }
             );
         }
+
+        @DisplayName("존재하지 않는 특정 문서의 Uuid로 요청한다면 예외가 발생한다 : DOCUMENT_NOT_FOUND")
+        @Test
+        void searchOrganizationDocument_error_byNonExistingDocumentUuid() {
+            // when & then
+            WikiException ex = assertThrows(WikiException.class,
+                    () -> documentService.searchOrganizationDocument(UUID.randomUUID()));
+            Assertions.assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.DOCUMENT_NOT_FOUND);
+        }
     }
 
     @DisplayName("특정 문서에 대해 조직 문서를 추가할 때에")
     @Nested
     class addOrganizationDocument {
 
+        private DocumentOrganizationAddRequest documentOrganizationAddRequest;
+
+        @BeforeEach
+        void setUp() {
+            documentOrganizationAddRequest = OrganizationDocumentFixture.createDocumentOrganizationDocumentCreateRequest(
+                    "title1", "defaultContents", "defaultWriter", 10L, UUID.randomUUID());
+        }
+
         @DisplayName("존재하는 특정 문서의 uuid로 요청한다면, 조직 문서가 추가된다")
         @Test
         void addOrganizationDocument_success_byExistingDocumentUuid() {
-            // given
-            DocumentOrganizationAddRequest documentOrganizationAddRequestDefault = OrganizationDocumentFixture.createDocumentOrganizationDocumentCreateRequest(
-                    "title1", "defaultContents", "defaultWriter", 10L, UUID.randomUUID());
-
             // when
             documentService.addOrganizationDocument(savedDocumentUuid,
-                    documentOrganizationAddRequestDefault);
+                    documentOrganizationAddRequest);
 
             // then
             List<OrganizationDocumentResponse> organizationDocumentResponsesByDocument = documentOrganizationLinkService.findOrganizationDocumentResponsesByDocument(
@@ -112,6 +129,16 @@ class DocumentServiceJavaTest {
                         ).containsExactlyInAnyOrder("title1");
                     }
             );
+        }
+
+
+        @DisplayName("존재하지 않는 특정 문서의 Uuid로 요청한다면 예외가 발생한다 : DOCUMENT_NOT_FOUND")
+        @Test
+        void addOrganizationDocument_error_byNonExistingDocumentUuid() {
+            // when & then
+            WikiException ex = assertThrows(WikiException.class,
+                    () -> documentService.addOrganizationDocument(UUID.randomUUID(), documentOrganizationAddRequest));
+            Assertions.assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.DOCUMENT_NOT_FOUND);
         }
     }
 
@@ -143,6 +170,24 @@ class DocumentServiceJavaTest {
                     savedDocument);
 
             assertThat(organizationDocumentResponsesByDocument.size()).isEqualTo(0);
+        }
+
+        @DisplayName("존재하지 않는 특정 문서의 Uuid로 요청한다면 예외가 발생한다 : DOCUMENT_NOT_FOUND")
+        @Test
+        void deleteOrganizationDocument_error_byNonExistingDocumentUuid() {
+            // when & then
+            WikiException ex = assertThrows(WikiException.class,
+                    () -> documentService.deleteOrganizationDocument(UUID.randomUUID(), savedOrganizationDocumentUuid));
+            Assertions.assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.DOCUMENT_NOT_FOUND);
+        }
+
+        @DisplayName("존재하지 않는 특정 조직 문서의 Uuid로 요청한다면 예외가 발생한다 : ORGANIZATION_DOCUMENT_NOT_FOUND")
+        @Test
+        void deleteOrganizationDocument_error_byNonExistingOrganizationDocumentUuid() {
+            // when & then
+            WikiException ex = assertThrows(WikiException.class,
+                    () -> documentService.deleteOrganizationDocument(savedDocumentUuid, UUID.randomUUID()));
+            Assertions.assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.ORGANIZATION_DOCUMENT_NOT_FOUND);
         }
     }
 }
