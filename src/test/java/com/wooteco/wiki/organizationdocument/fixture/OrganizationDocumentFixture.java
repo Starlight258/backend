8765cdc413d8728fad6f533dc114a6ce5f6fef84
2jin2.1031 <2jin2.1031@gmail.com>
2025-08-30 16:37:14 +0900

    test: 불필요한 메서드 제거

--- src/test/java/com/wooteco/wiki/organizationdocument/fixture/OrganizationDocumentFixture.java
+++ src/test/java/com/wooteco/wiki/organizationdocument/fixture/OrganizationDocumentFixture.java
@@ -2,7 +2,6 @@ package com.wooteco.wiki.organizationdocument.fixture;
 
 import com.wooteco.wiki.document.dto.DocumentOrganizationMappingAddRequest;
 import com.wooteco.wiki.organizationdocument.domain.OrganizationDocument;
-import com.wooteco.wiki.organizationdocument.dto.request.DocumentOrganizationDocumentLinkCreateRequest;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -15,28 +14,6 @@ public class OrganizationDocumentFixture {
 
     public static OrganizationDocument createDefault() {
         return create("defaultTitle", "defaultContent", "defaultWriter", 10L, UUID.randomUUID(), LocalDateTime.now());
-    }
-
-    public static DocumentOrganizationDocumentLinkCreateRequest createOrganizationDocumentCreateRequest(
-            String title,
-            String contents,
-            String writer,
-            Long documentBytes,
-            UUID uuid,
-            UUID documentUUID
-    ) {
-        return new DocumentOrganizationDocumentLinkCreateRequest(title, contents, writer, documentBytes, uuid, documentUUID);
-    }
-
-    public static DocumentOrganizationDocumentLinkCreateRequest createOrganizationDocumentCreateRequestDefault(UUID documentUUID) {
-        return createOrganizationDocumentCreateRequest("defaultTitle", "defaultContent", "defaultWriter", 10L,
-                UUID.randomUUID(), documentUUID);
-    }
-
-    public static DocumentOrganizationDocumentLinkCreateRequest createOrganizationUpdateRequest(String title, String contents,
-                                                                                                String writer, Long documentBytes) {
-        return new DocumentOrganizationDocumentLinkCreateRequest(title, contents, writer, documentBytes, UUID.randomUUID(),
-                UUID.randomUUID());
     }
 
     public static DocumentOrganizationMappingAddRequest createDocumentOrganizationDocumentCreateRequest(String title, String contents, String writer, Long documentBytes, UUID uuid) {
