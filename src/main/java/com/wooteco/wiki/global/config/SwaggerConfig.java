e268e424e5cc72ab3e7c6fe6e0f9b107aa7e8633
Mint <audwl03071@gmail.com>
2025-08-31 23:00:35 +0900

    fix: Swagger https 스킴 설정

--- src/main/java/com/wooteco/wiki/global/config/SwaggerConfig.java
+++ src/main/java/com/wooteco/wiki/global/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.wooteco.wiki.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,10 @@ public class SwaggerConfig {
 
         Info info = new Info().title(title).description(description).version("1.0.0");
 
-        return new OpenAPI().info(info);
+        Server server = new Server();
+        server.setUrl("https://api.crew-wiki.site");
+        server.setDescription("Prod server");
+
+        return new OpenAPI().info(info).servers(java.util.List.of(server));
     }
 }
