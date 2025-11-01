9420b34df94b5e22a6a114d894623a883dec92e0
2jin2.1031 <2jin2.1031@gmail.com>
2025-11-01 21:28:28 +0900

    feat: cors, swagger 설정 프로퍼티 처리

--- src/main/java/com/wooteco/wiki/global/config/SwaggerConfig.java
+++ src/main/java/com/wooteco/wiki/global/config/SwaggerConfig.java
@@ -4,11 +4,15 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger.server-url}")
+    private String serverUrl;
 
     @Bean
     public OpenAPI roomEscapeOpenAPI() {
@@ -18,8 +22,7 @@ public class SwaggerConfig {
         Info info = new Info().title(title).description(description).version("1.0.0");
 
         Server server = new Server();
-        server.setUrl("https://api.crew-wiki.site");
-        server.setDescription("Prod server");
+        server.setUrl(serverUrl);
 
         return new OpenAPI().info(info).servers(List.of(server));
     }
